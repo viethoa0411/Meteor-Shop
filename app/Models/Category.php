309fdffac91ef324499005f507d59c66e1ede470
duymanh9 <manhbe417@gmail.com>
2025-12-04 21:39:52 +0700

    Tạo model cho chức năng thêm, sửa, xóa ảnh ở trang chủ client

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,6 +10,8 @@ class Category extends Model
     protected $fillable = [
         'name',
         'slug',
+        'description',
+        'image',
         'parent_id',
         'status'
     ];
