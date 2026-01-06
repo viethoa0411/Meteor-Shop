cbad1fb5ef0b093f2a4e9d7f20bff61e22636a0d
dodinhtruong <dodinhtruong852005@gmail.com>
2026-01-06 10:32:42 +0700

    thêm softdeletes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Wishlist;
 
 
 
 class Product extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'name',
         'slug',
